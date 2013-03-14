db19aa6a92eb1e29d736bba3fdf5f50afcc06038
kctang <kctang@big2.net>
2013-03-14 08:09:46 +0800

    Update build.sh to copy optimized www to android & ios's www directory

--- src/build.sh
+++ src/build.sh
@@ -2,3 +2,8 @@ node r.js -o www/js/build.js
 
 # remove components directory in android/assets/www
 rm -fr ../android/assets/www/components
+rm -fr ../android/assets/www/lib/jquery.mobile-1.3.0/demos
+
+# copy to ios/www
+rm -fr ../ios/www/*
+cp -r ../android/assets/www/* ../ios/www
